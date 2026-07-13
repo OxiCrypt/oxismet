b3a2bb97dcc584779116fe411271be8199ae4c7c
Rage Fade <ragef6@tutanota.de>
2026-07-13 15:07:16 -0400

    Added versioning

--- src/bin/passwd.rs
+++ src/bin/passwd.rs
@@ -2,6 +2,8 @@ use std::{fs::File, io::Write, process::ExitCode};
 
 use smet::{GcmNonce, Salt};
 
+use crate::VERSION;
+
 pub fn encrypt_with_password(
     password: &str,
     bytes: &[u8],
@@ -9,6 +11,9 @@ pub fn encrypt_with_password(
 ) -> Result<(), ExitCode> {
     let (encrypted_data, salt) =
         smet::encrypt_with_password(bytes, password).map_err(|_| ExitCode::FAILURE)?;
+    buffer
+        .write_all(&VERSION.to_be_bytes())
+        .map_err(|_| ExitCode::FAILURE)?;
     buffer
         .write_all(encrypted_data.nonce.as_slice())
         .map_err(|_| ExitCode::FAILURE)?;
@@ -25,8 +30,15 @@ pub fn decrypt_with_password(
     bytes: &[u8],
     buffer: &mut File,
 ) -> Result<(), ExitCode> {
-    let nonce = &bytes[0..12];
-    let salt = &bytes[12..28];
+    let version = u64::from_be_bytes(
+        bytes[0..8].try_into().map_err(|_| ExitCode::FAILURE)?, /* This Error is impossible, 8 = 8 */
+    );
+    if version != VERSION {
+        eprintln!("Error: Unsupported File Version: {version}.");
+        return Err(ExitCode::FAILURE);
+    }
+    let nonce = &bytes[8..20];
+    let salt = &bytes[20..36];
     let plaintext = smet::decrypt_with_password(
         bytes,
         password,
@@ -34,7 +46,7 @@ pub fn decrypt_with_password(
             salt.try_into().map_err(|_| ExitCode::FAILURE)?, /* This Error is impossible, 16 = 16 */
         ),
         &GcmNonce::from_slice(
-            nonce.try_into().map_err(|_| ExitCode::FAILURE)?, /* This Error is impossible 12 = 12 */
+            nonce.try_into().map_err(|_| ExitCode::FAILURE)?, /* This Error is impossible, 12 = 12 */
         ),
     )
     .map_err(|_| ExitCode::FAILURE)?;
